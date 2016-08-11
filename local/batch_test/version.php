3245e0673fb5b39646fce3f9263f9d461346ebbe
ahwelp <arturh@viavale.com.br>
2016-08-11 14:33:30 -0300

    First commit

---
+++ local/batch_test/version.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Google Tests
+ *
+ * @package    local_google_test
+ * @author     Artur Welp <ahwelp@univates.br>
+ * @author     Maurício Severo da Silva <mss@univates.br>
+ * @author     Alexandre Sturmer Wolf <awolf@univates.br>
+ * @copyright  2016 Univates - htttp://www.univates.br/
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$plugin->version   = 2016110801;
+$plugin->requires  = 2014051200; // Moodle 2.7.0 is required.
+$plugin->component = 'local_google_tests';
+$plugin->maturity  = MATURITY_ALPHA; 
+
+
